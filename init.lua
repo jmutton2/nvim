b13c8529af44906553dabe1c7863c1a3cbffa142
Jacob Mutton <jmuton2@uwo.ca>
2024-03-08 19:00:45 -0500

    Added some snippets and a nice highlight on yank

--- init.lua
+++ init.lua
@@ -2,3 +2,17 @@ require('core.plugins')
 require('core.keymaps')
 require('core.plugin_config')
 
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -36,21 +36,27 @@ return require('packer').startup(function(use)
 		branch = 'v3.x',
 		requires = {
 			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			{'neovim/nvim-lspconfig'},
 			{'williamboman/mason.nvim'},
 			{'williamboman/mason-lspconfig.nvim'},
 
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
 			-- Autocompletion
 			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
 			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-nvim-lua'},
+
+			-- Snippets
 			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'},
 		}
 	}
 
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
-
+	use('ThePrimeagen/harpoon')
 
 
 	-- Automatically set up your configuration after cloning packer.nvim
