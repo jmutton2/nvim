a2520767044f9396868c8e9701a1062e9d108c71
Jacob Mutton <jmuton2@uwo.ca>
2024-07-28 20:17:13 -0400

    Remove go

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "javascript", "typescript", "svelte", "go", "ruby", "rust", "python" },
+  ensure_installed = { "javascript", "typescript", "svelte", "ruby", "rust", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
